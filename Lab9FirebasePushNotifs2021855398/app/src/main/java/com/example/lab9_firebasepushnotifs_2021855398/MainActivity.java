5852d8bfbd5ea4a1e85d44dffb215c195d0f9547
Haziq Aiman <haziqaiman2@gmail.com>
2022-01-31 22:16:01 +0800

    lab 9 - 2021855398

---
+++ Lab9FirebasePushNotifs2021855398/app/src/main/java/com/example/lab9_firebasepushnotifs_2021855398/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab9_firebasepushnotifs_2021855398;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
